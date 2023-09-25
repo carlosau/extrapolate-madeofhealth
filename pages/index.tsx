6be2fcd197742fee9edb0cec9d2cf63524d4437a
Carlos Souza <carlosau@pop-os.localdomain>
2023-09-25 18:50:22 -0300

    changed input and output photos links

--- pages/index.tsx
+++ pages/index.tsx
@@ -72,9 +72,9 @@ export default function Home({ count }: { count: number }) {
           </p>
         </motion.div>
         <PhotoBooth
-          input={`${process.env.NEXT_PUBLIC_CLOUDFLARE_WORKER}/input.webp`}
+          input="https://myskin.madeofhealth.com/input.webp"
           blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAQAAAADCAIAAAA7ljmRAAAACXBIWXMAAAsTAAALEwEAmpwYAAAAMklEQVR4nAEnANj/ALjj/4mIh+P+/9Lv/wCn0+xeLxV9cWWUtL0AUz0tKQAAeVU0j4d/y2cTsDiuaawAAAAASUVORK5CYII="
-          output={`${process.env.NEXT_PUBLIC_CLOUDFLARE_WORKER}/output.gif`}
+          output="https://myskin.madeofhealth.com/output.gif"
         />
       </motion.div>
     </Layout>
