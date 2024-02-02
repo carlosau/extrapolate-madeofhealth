1285ad028fc41ab7c3223374d366160ffef1c790
Carlos Souza <carlosaugsz@hotmail.com>
2024-02-02 10:55:23 -0300

    removed motion-frame

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,5 @@
 import Layout from "@/components/layout";
 import Balancer from "react-wrap-balancer";
-import { motion } from "framer-motion";
-import { FADE_DOWN_ANIMATION_VARIANTS } from "@/lib/constants";
 import { useUploadModal } from "@/components/home/upload-modal";
 import { Upload, Heart } from "lucide-react";
 import PhotoBooth from "@/components/home/photo-booth";
@@ -35,30 +33,19 @@ export default function Home({ count }: { count: number }) {
         <a href="/privacy-notice">Privacy Notice</a>.
       
       </CookieConsent>
-      <motion.div
+      <div
         className="z-10 max-w-2xl px-5 xl:px-0"
-        initial="hidden"
-        whileInView="show"
-        animate="show"
-        viewport={{ once: true }}
-        variants={{
-          hidden: {},
-          show: {
-            transition: {
-              staggerChildren: 0.15,
-            },
-          },
-        }}
+      
       >
-        <motion.h1
+        <h1
           className="bg-gradient-to-br from-black to-stone-500 bg-clip-text text-center font-display text-4xl font-bold tracking-[-0.02em] text-transparent drop-shadow-sm md:text-7xl md:leading-[5rem]"
-          variants={FADE_DOWN_ANIMATION_VARIANTS}
+          
         >
           <Balancer>How will your facial skin look?</Balancer>
-        </motion.h1>
-        <motion.p
+        </h1>
+        <p
           className="mt-6 text-center text-gray-500 md:text-xl"
-          variants={FADE_DOWN_ANIMATION_VARIANTS}
+          
         >
           <Balancer ratio={0.6}>
             Upload your photo, view the results and discover a special product¹ for your skin.{" "}
@@ -82,8 +69,8 @@ export default function Home({ count }: { count: number }) {
             </Tooltip>
             */}
           </Balancer>
-        </motion.p>
-        <motion.div variants={FADE_DOWN_ANIMATION_VARIANTS} className="-mb-4">
+        </p>
+        <div className="-mb-4">
           <button
             className="group mx-auto mt-6 flex max-w-fit items-center justify-center space-x-2 rounded-full bg-lime-400 px-5 py-2 text-sm text-white font-bold transition-colors hover:bg-lime-300 hover:text-black"
             onClick={() => setShowUploadModal(true)}
@@ -94,13 +81,13 @@ export default function Home({ count }: { count: number }) {
           <p className="collapse mt-6 text-center text-sm text-gray-500">
             {nFormatter(count)} fotos geradas e contando...
           </p>
-        </motion.div>
+        </div>
         <PhotoBooth
           input={`${process.env.NEXT_PUBLIC_CLOUDFLARE_WORKER}/input.webp`} 
           blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAQAAAADCAIAAAA7ljmRAAAACXBIWXMAAAsTAAALEwEAmpwYAAAAMklEQVR4nAEnANj/ALjj/4mIh+P+/9Lv/wCn0+xeLxV9cWWUtL0AUz0tKQAAeVU0j4d/y2cTsDiuaawAAAAASUVORK5CYII="
           output={`${process.env.NEXT_PUBLIC_CLOUDFLARE_WORKER}/output.gif`}
         />
-      </motion.div>
+      </div>
     </Layout>
     </>
   );
